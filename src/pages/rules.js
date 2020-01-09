64354249f7ca1c7843f62cf0e55262982e96a41a
Vladyslav <ganushkevychv@gmail.com>
2020-01-09 22:10:46 +0200

    rules page created

---
+++ src/pages/rules.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const contactsPage = () => (
+  <Layout>
+    <SEO title="Contacts" />
+    <h1>Руководство</h1>
+    <p>Welcome to page 2</p>
+    
+  </Layout>
+)
+
+export default contactsPage
